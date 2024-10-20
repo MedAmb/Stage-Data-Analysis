9d2322ca9e12260d9ec749ea80447b943f940842
Khadija <khadisenh@gmail.com>
2024-10-20 11:05:36 +0000

    datacollection

--- DataCollection/twitter.py
+++ DataCollection/twitter.py
@@ -1,12 +1,62 @@
+import sys
+import os 
+from datetime import datetime, timedelta
+import time
+# le chemin du dossier 
+sys.path.append(os.path.abspath('../External/selenium-twitter-scraper'))
+
 # 1. importer twitter scraper
+from twitter_scraper import Twitter_Scraper
 
-# 2. initialiser des variables user_name user_emai, password
+# 2. initialiser des variables user_name, user_mail, password
 
-# 3. initialiser un objet twitter scraper 
+user_name='KSenhoury'
+user_mail='khadisenh@gmail.com' 
+Password='Taghle63'
+ 
+# 3. Initialiser un objet twitter scraper
+
+My_scrape=Twitter_Scraper(mail=user_mail,username=user_name, password=Password,
+max_tweets=1000, scrape_top= True)
 
 # 4. utiliser l'objet twitter scraper pour connecter a twitter 
+My_scrape.login()
 
-# 5. creer string qui pourra nous donner  100 tweet qui ont le  hachttag #gaza pour chaque moi de 
-#0ct 2023-oct2024 en 2 langues arabe et anglais 
 
-# 6. utiliser l'objet twitter scraper pour telecharger les tweets 
+# 5. creer string qui pourra nous donner  1000 tweet qui ont le  hachttag #gaza pour chaque mois de 
+#07oct2023-07oct2024 en 2 langues arabe et anglais 
+
+start_date = datetime(2024, 10, 18)
+end_date = datetime(2023, 10, 7)
+query_base = '(Gaza OR palestine OR 07oct OR israel) '
+M = 'Q'
+while M!='Q':
+    print('query base')
+    query_base = input()
+    print(query_base)
+    print('confirmer:y')
+    c = input()
+    if c == 'y':
+        My_scrape.scrape_tweets(scrape_query=query_base)
+    print('quitter?')
+    M = input()
+
+
+# Boucle à travers chaque mois
+while start_date > end_date:
+    next_month = start_date - timedelta(days=30)  # On ajoute un mois
+    month_end = max(next_month, end_date)
+    Query= query_base + 'lang:en ' + f'since:{next_month} '.split(' ')[0] + ' ' + f'until:{start_date} '.split(' ')[0]
+    Query_AR= query_base + 'lang:ar ' + f'since:{next_month} '.split(' ')[0] + ' ' + f'until:{start_date} '.split(' ')[0]
+
+    # Passer au mois suivant 
+    start_date=next_month
+    print('scraping tweets for query ', Query) 
+
+    My_scrape.scrape_tweets(scrape_query=Query,max_tweets=1000)
+    My_scrape.save_to_csv()
+    time.sleep(10)
+    print('scraping tweets for query ', Query) 
+    My_scrape.scrape_tweets(scrape_query=Query_AR,max_tweets=1000)
+    My_scrape.save_to_csv()
+    time.sleep(10)
--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,4 @@
 #HowTo
 
 1. Comment entrer l'environement de travail
-
-    - docker run -it --rm --privileged --network=host -v C:\Users\smrc\OneDrive\Dokumenti\projet\stage:/home/stage internshipenv
+ docker run -it --rm --privileged --network=host -v C:\Users\smrc\OneDrive\Dokumenti\projet\stage:/home/stage internshipenv
