882ab6249b144090ac5a45207a66f71067faec39
medamb <ahmedmbadimed@gmail.com>
2024-10-24 19:30:15 +0200

    Initialiser la partie analyse

---
+++ DataAnalysis/Analyzer.py
@@ -0,0 +1,8 @@
+# Importer les libraries nécessaires
+
+# Creer Une Analyzeur qui GPT 3 pourra utiliser pour analyser les tweets
+
+# __init__ prends en parametre le prompt initial qui va données à GPT3 le contexte de la conversation
+
+# Analyze prends en parametre le texte à analyser et retourne le résultat de l'analyse. Le resultat a 4
+# valeur possible : "support", "oppose", "neutral", "not relevant"
---
+++ DataAnalysis/TweetAnalyzer.py
@@ -0,0 +1,21 @@
+# Importer le class Analyzer
+# Importer les libraries nécessaires
+
+# creer un class tweet qui prends en parametre le content du tweet, le nom de l'utilisateur, handle, la date du tweet
+# et le nombre de like, commentaires et retweets et la analyse du tweet
+
+# Utiliser la library os pour collecter les fichiers json dans le dossier ../DataCollection/tweets
+
+# Creer une condition pour processer que les tweets en anglais
+
+# Creer une boucle pour lire chaque fichier json
+
+# Pour chaque element dans le fichier json, creer un objet tweet et l'ajouter à une liste de tweets
+
+# Creer un objet Analyzer
+
+# Creer une boucle pour analyser chaque tweet
+
+# Ajouter le resultat de l'analyse à l'objet tweet
+
+# Creer un fichier json pour sauvegarder les tweets analysés
